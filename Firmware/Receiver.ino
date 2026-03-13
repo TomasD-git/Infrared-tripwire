69cb7ff7a26e2b201f97fe29e3426ac0c13c9981
loloy123wwwww <thomasdvorak76@gmail.com>
2026-03-13 20:35:42 +0100

    added both parts of firmware
    if you are a reviewer put smiley emoji at the end of your review, wanna see how deep you guys look

---
+++ Firmware/Receiver.ino
@@ -0,0 +1,90 @@
+// infrared tripwire
+// Receiver part
+#include <HijelHID_BLEKeyboard.h> // download this library or it wont work
+#define PIN_POWER_EN   2  
+#define PIN_IR_IN     34   
+#define PIN_LED_GREEN 16   
+#define PIN_LED_RED   17    
+#define PIN_BUTTON    33  
+#define DEBOUNCE_MS   50
+
+
+HijelHID_BLEKeyboard keyboard("IR Tripwire Receiver", "DIY");
+bool lastBeamState = false;   
+
+void setGreen() {
+  digitalWrite(PIN_LED_RED,   LOW);
+  digitalWrite(PIN_LED_GREEN, HIGH);
+}
+
+void setRed() {
+  digitalWrite(PIN_LED_GREEN, LOW);
+  digitalWrite(PIN_LED_RED,   HIGH);
+}
+
+void ledsOff() {
+  digitalWrite(PIN_LED_GREEN, LOW);
+  digitalWrite(PIN_LED_RED,   LOW);
+}
+
+void goToSleep() {
+  Serial.println("Going to deep sleep. Press button to wake.");
+  ledsOff();
+  delay(100);
+  esp_sleep_enable_ext0_wakeup((gpio_num_t)PIN_BUTTON, LOW);
+  esp_deep_sleep_start();
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(PIN_POWER_EN, OUTPUT);
+  digitalWrite(PIN_POWER_EN, HIGH);
+  pinMode(PIN_IR_IN,     INPUT);       
+  pinMode(PIN_LED_GREEN, OUTPUT);
+  pinMode(PIN_LED_RED,   OUTPUT);
+  pinMode(PIN_BUTTON,    INPUT_PULLUP);  
+
+  ledsOff();
+
+  setGreen(); delay(150);
+  ledsOff();  delay(100);
+  setGreen(); delay(150);
+  ledsOff();  delay(100);
+  keyboard.begin();
+  delay(500); 
+  bool beamBroken = (digitalRead(PIN_IR_IN) == HIGH);
+  lastBeamState   = beamBroken;
+  beamBroken ? setRed() : setGreen();
+}
+
+void loop() {
+  if (digitalRead(PIN_BUTTON) == LOW) {
+    delay(DEBOUNCE_MS);
+    if (digitalRead(PIN_BUTTON) == LOW) { 
+      delay(200);                          
+      goToSleep();                         
+    }
+  }
+  bool beamBroken = (digitalRead(PIN_IR_IN) == HIGH);  
+
+  if (beamBroken != lastBeamState) {
+    lastBeamState = beamBroken;
+
+    if (beamBroken) {
+      setRed();
+
+      if (keyboard.isConnected()) {
+        keyboard.press(KEY_TAB, KEY_MOD_LALT);
+        delay(25);
+        keyboard.releaseAll();
+      } else {
+        Serial.println("PC not connected");
+      }
+
+    } else {
+      setGreen();
+    }
+  }
+
+  delay(10);
+}
---
+++ Firmware/Transmitter.ino
@@ -0,0 +1,46 @@
+// IR tripwire
+// transmitter part
+#define PIN_POWER_EN   2   
+#define PIN_IR_LED     5   
+#define PIN_LED_GREEN 16   
+#define PIN_BUTTON    33 
+#define IR_FREQ_HZ      38000   
+#define IR_RESOLUTION   8       
+#define IR_DUTY_50PCT   128    
+#define DEBOUNCE_MS   50
+
+
+void goToSleep() {
+  ledcWrite(PIN_IR_LED, 0);
+  ledcDetach(PIN_IR_LED);
+  digitalWrite(PIN_IR_LED, LOW);
+  digitalWrite(PIN_LED_GREEN, LOW);
+  delay(100);
+  esp_sleep_enable_ext0_wakeup((gpio_num_t)PIN_BUTTON, LOW);
+  esp_deep_sleep_start();
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(PIN_POWER_EN, OUTPUT);
+  digitalWrite(PIN_POWER_EN, HIGH);
+  pinMode(PIN_LED_GREEN, OUTPUT);
+  pinMode(PIN_BUTTON,    INPUT_PULLUP); 
+  pinMode(PIN_IR_LED,    OUTPUT);
+  ledcAttach(PIN_IR_LED, IR_FREQ_HZ, IR_RESOLUTION);
+  ledcWrite(PIN_IR_LED, IR_DUTY_50PCT);  
+  digitalWrite(PIN_LED_GREEN, HIGH);
+}
+
+void loop() {
+
+  if (digitalRead(PIN_BUTTON) == LOW) {
+    delay(DEBOUNCE_MS);
+    if (digitalRead(PIN_BUTTON) == LOW) { 
+      delay(200);                        
+      goToSleep();                       
+    }
+  }
+
+  delay(10);
+}
